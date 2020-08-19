7d4bffc72b000f8afcdd9649e679004409a77a41
hexaludira <hexaludira@gmail.com>
2020-08-19 14:44:03 +0700

    Add delete dialog alert

--- lib/home.dart
+++ lib/home.dart
@@ -10,96 +10,138 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-   CRUD dbHelper = CRUD();
-      Future<List<ClassPenangkap>> future;
+  CRUD dbHelper = CRUD();
+  Future<List<ClassPenangkap>> future;
+  AlertDialog alert;
 
-      @override
-      void initState() {
-        super.initState();
-        updateListView();
-      }
+  @override
+  void initState() {
+    super.initState();
+    updateListView();
+  }
 
-      void updateListView() {
-        setState(() {
-          future = dbHelper.getContactList();
-        });
-      }
+  void updateListView() {
+    setState(() {
+      future = dbHelper.getContactList();
+    });
+  }
 
-      Future<ClassPenangkap> navigateToEntryForm(
-        BuildContext context, ClassPenangkap contact) async {
-          var result = await Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-            return EntryForm(contact);
-          }));
-          return result;
-        }
+  showAlertDialog(BuildContext context) {
+    ClassPenangkap contact;
+    //button OK
+    Widget yesButton = FlatButton(
+      onPressed: () async {
+        int result = await dbHelper.delete(contact);
+        if (result > 0) {
+          updateListView();
+        } 
 
-        Card cardo(ClassPenangkap contact) {
-          // if (contact.status == )
-          return Card(
-            color: Colors.white,
-            elevation: 2.0,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.blueGrey[300],
-                child: Icon(Icons.chrome_reader_mode),
-            ),
-            title: Text(
-              contact.date,
-            ),
-            subtitle: Text(contact.detail.toString() + " || " + contact.location.toString()),
-            trailing: GestureDetector(
-              child: Icon(Icons.delete),
-              onTap: () async {
-                int result = await dbHelper.delete(contact);
-                if (result > 0) {
-                  updateListView();
-                }
-              },
-            ),
-            onTap: () async {
-              var contact2 = await navigateToEntryForm(context, contact);
-              if (contact2 != null){
-                int result = await dbHelper.update(contact2);
-                if (result > 0) {
-                  updateListView();
-                }
-              } 
-            },
-            ),
+      },
+      child: Text("Alrighty"),
+    );
 
-          );
-        }
+    Widget cancelButton = FlatButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: Text("Nope"),
+    );
+
+    //persiapan alert dialog
+    alert = AlertDialog(
+      title: Text("Delete Confirmation"),
+      content: Text("Are you sure you want to destroy this item?"),
+      actions: [
+        yesButton,
+        cancelButton,
+      ],
+    );
+
+    //menampilkan dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+      return alert;
+    });
+  }
+
+  Future<ClassPenangkap> navigateToEntryForm(
+      BuildContext context, ClassPenangkap contact) async {
+    var result = await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) {
+      return EntryForm(contact);
+    }));
+    return result;
+  }
+
+  Card cardo(ClassPenangkap contact) {
+    // if (contact.status == )
+    return Card(
+      color: Colors.white,
+      elevation: 2.0,
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.blueGrey[300],
+          child: Icon(Icons.chrome_reader_mode),
+        ),
+        title: Text(
+          contact.date,
+        ),
+        subtitle: Text(
+            contact.detail.toString() + " || " + contact.location.toString()),
+        trailing: GestureDetector(
+          child: Icon(Icons.delete),
+          onTap: () {
+            //show alert dialog
+            showAlertDialog(context);
+            // int result = await dbHelper.delete(contact);
+            // if (result > 0) {
+            //   updateListView();
+            // }
+          },
+        ),
+        onTap: () async {
+          var contact2 = await navigateToEntryForm(context, contact);
+          if (contact2 != null) {
+            int result = await dbHelper.update(contact2);
+            if (result > 0) {
+              updateListView();
+            }
+          }
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       title: Text('List Metal Problem'),
-     ),
-     body: FutureBuilder<List<ClassPenangkap>>(
-       future: future,
-       builder: (context, snapshot) {
-         if(snapshot.hasData) {
-           return Column(
-             children: snapshot.data.map((todo) => cardo(todo)).toList());
-         } else {
-           return SizedBox();
-         }
-       },
-     ),
-     floatingActionButton: FloatingActionButton(
-       child: Icon(Icons.add),
-       tooltip: 'Tambah Data',
-       onPressed: () async {
-         var contact = await navigateToEntryForm(context, null);
-         if (contact != null) {
-           int result = await dbHelper.insert(contact);
-           if (result > 0) {
-             updateListView();
-           }
-         }
-       }),
-
+      appBar: AppBar(
+        title: Text('List Metal Problem'),
+      ),
+      body: FutureBuilder<List<ClassPenangkap>>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+                children: snapshot.data.map((todo) => cardo(todo)).toList());
+          } else {
+            return SizedBox();
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          tooltip: 'Tambah Data',
+          onPressed: () async {
+            var contact = await navigateToEntryForm(context, null);
+            if (contact != null) {
+              int result = await dbHelper.insert(contact);
+              if (result > 0) {
+                updateListView();
+              }
+            }
+          }),
     );
   }
 }
